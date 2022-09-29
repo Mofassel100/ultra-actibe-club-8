4365b64aae1b588300a28481438ca50d655f6fa1
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-09-29 09:45:39 +0600

    data pass other componer

--- src/Study/Study/Study.js
+++ src/Study/Study/Study.js
@@ -6,17 +6,17 @@ import React, { useEffect, useState } from 'react';
 import StudyInfor from './StudyInfor/StudyInfor';
 
 
-const Study = () => {
-    const [studyInfo, setStudyInfor] = useState([]);
+const Study = ({studyInfo}) => {
+    // const [studyInfo, setStudyInfor] = useState([]);
 
-    useEffect(() => {
-        fetch('studyActivies.json')
-            .then(res => res.json())
-            .then(data => setStudyInfor(data))
+    // useEffect(() => {
+    //     fetch('studyActivies.json')
+    //         .then(res => res.json())
+    //         .then(data => setStudyInfor(data))
 
 
 
-    }, [])
+    // }, [])
     return (
         <div>
             <div>
--- src/StudyActivies/StudyActivies.js
+++ src/StudyActivies/StudyActivies.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PersonalInform from '../PersonalInform/PersonalInform';
 import Study from '../Study/Study/Study';
 
 import './StydyActivies.css'
 
 const StudyActivies = () => {
+    const [studyInfo,setStudyInfor]= useState([])
+    useEffect(()=>{
+        
+     fetch('studyActivies.json')
+     .then(res => res.json())
+     .then(data => setStudyInfor(data))
+    
+
+    },[])
+
+
     return (
         <div className='row'>
            <div className=' study container'>
            
-            <Study></Study>
+            <Study studyInfo={studyInfo}></Study>
             
             </div> 
            <div className='bg-red-500 aside'>
