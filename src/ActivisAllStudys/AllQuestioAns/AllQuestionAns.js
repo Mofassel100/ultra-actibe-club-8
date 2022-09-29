425787a9da2622f15e2cd7e047d8687d01867c90
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-09-29 22:40:55 +0600

    three question added

--- src/ActivisAllStudys/AllQuestioAns/AllQuestionAns.js
+++ src/ActivisAllStudys/AllQuestioAns/AllQuestionAns.js
@@ -7,6 +7,16 @@ const AllQuestionAns = () => {
   <div className="card-body">
     <h2 className="card-title">How react works ? </h2>
     <p><span>Ans</span> React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes. </p>
+    <h2 className="card-title">Showing results for props and useState between difference ? </h2>
+    <p><span>Ans</span>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. </p>
+    <h2 className="card-title">how react useEffect wtih out data load working ? </h2>
+    <p><span>Ans</span> useEffect use cases
+Running once on mount: fetch API data
+Running on state change: validating input field
+Running on state change: live filtering
+Running on state change: trigger animation on new array value
+Running on props change: update paragraph list on fetched API data update
+Running on props change: updating fetched API data to get BTC updated price </p>
     <div className="card-actions justify-end">
    
       <button className="btn btn-primary">Buy Now</button>
